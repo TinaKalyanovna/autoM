83ceb5fc4912f5769332714bd20b89f4f82e1f78
Tina <fatina.kalyanovna@gmail.com>
2024-04-17 22:42:05 +0300

    Initial commit

---
+++ HW_6/src/main.java.homework/GoToMainFromCartButton.java
@@ -0,0 +1,17 @@
+import io.appium.java_client.AppiumDriver;
+import homework.screens.CartScreen;
+
+
+public class GoToMainFromCartButton implements GoToMainScreenStrategy {
+    AppiumDriver<?> driver;
+
+    public GoToMainFromCartButton(AppiumDriver<?> driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void goToMain() {
+        CartScreen cartScreen = new CartScreen(driver);
+        cartScreen.goToMain();
+    }
+}
